1283dd15182c803cc9bb55bfdb3436a5d9bf3d30
Matt Pagan <matthew.pagan@me.com>
2015-11-04 23:50:38 -0500

    Fixed error in facebook.coffee.erb file which was causing javascript error

--- Gemfile
+++ Gemfile
@@ -52,6 +52,8 @@ gem 'omniauth'
 
 gem 'omniauth-facebook'
 
+gem 'coffee-script-source', '1.8.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,15 @@ class User < ActiveRecord::Base
    def self.from_omniauth(auth)
     Rails.logger.info "\n\ninside model info: #{auth.info.inspect}\n\n"
   	 where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  	    #so clearly we can't use this email forever 
-        #it is just acting as proof that if facebook provides us with an email 
-        #that our user DOES get rgistered.
-        #next up - sleuthe on solutions for why facebook doesnt provide us with email 
-        #and/or the fix for it
+  	    # so clearly we can't use this email forever 
+        # it is just acting as proof that if facebook provides us with an email 
+        # that our user DOES get registered.
+        # next up - sleuthe on solutions for why facebook doesnt provide us with email 
+        # and/or the fix for it
         user.email = 'testemail@test.com'
   	    user.password = Devise.friendly_token[0,20]
   	    user.name = auth.info.name   # assuming the user model has a name
-        #set users email as confirmed so that you get logged in for real
+        # set users email as confirmed so that you get logged in for real
 
   	 end
    end
